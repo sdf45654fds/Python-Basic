5aa7370a834b9aefc49c1bf9b672ae59e23dc64c
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2024-06-03 10:21:05 +0900

    Add files via upload

---
+++ 자료형/02_변수.py
@@ -0,0 +1,20 @@
+''' 
+--변수선언--
+변수 이름 = 값
+'''
+envelope1 = 10000
+envelope2 = 20000
+envelope3 = '파이팅'
+
+print(envelope1)
+print(envelope2)
+print(envelope3)
+
+'''
+--변수 이름 규칙--
+문자 또는 _ 로 시작
+문자, 숫자, _로 구성
+공백 , 특수문자 X
+대소문자 구분
+키워드(예약어)X
+'''
